886b208513bedc72dcf25929a799ad77b2275802
Hồ Chí Tài <hochitai24@gmail.com>
2023-01-26 17:37:13 +0700

    header 1

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,12 +1,29 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Header.mdule.scss";
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx("wrapper")}>
-            <div className={cx("inner")}></div>
+            <div className={cx("inner")}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx("search")}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx("clear")}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx("loading")} />
+                    <button className={cx("search-btn")}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx("action")}></div>
+            </div>
         </header>
     );
 }
