08f158f49ccdbd81a831385ed11a588f48043fce
Hồ Chí Tài <hochitai24@gmail.com>
2023-01-29 19:25:01 +0700

    header 6

--- tiktok-ui/src/components/Popper/Menu/index.js
+++ tiktok-ui/src/components/Popper/Menu/index.js
@@ -38,6 +38,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
         <Tippy
             interactive
             delay={[0, 700]}
+            offset={[12, 8]}
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
@@ -53,7 +54,8 @@ function Menu({ children, items = [], onChange = defaultFn }) {
                         {renderItem()}
                     </PopperWrapper>
                 </div>
-            )}>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}>
             {children}
         </Tippy>
     );
